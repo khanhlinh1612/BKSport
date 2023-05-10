cf9cb83941095e235b487e8821385d8f2dadf139
Mạnh Hào <102290243+mhaoo@users.noreply.github.com>
2023-05-10 11:49:50 +0700

    Add select time for pump

--- src/admin/AddPumpTask.js
+++ src/admin/AddPumpTask.js
@@ -11,10 +11,27 @@ import {
 import Icon from "react-native-vector-icons/FontAwesome";
 import { Picker } from "@react-native-picker/picker";
 import LottieView from "lottie-react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import moment from "moment";
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
 export default AddPumpTask = function ({ navigation }) {
+  const showTimePicker = () => {
+    setTimePickerVisible(true);
+  };
+
+  const handleTimeConfirm = (time) => {
+    setSelectedTime(time);
+    setTimePickerVisible(false);
+  };
+
+  const handleTimeCancel = () => {
+    setTimePickerVisible(false);
+  };
+
+  const [isTimePickerVisible, setTimePickerVisible] = useState(false);
+  const [selectedTime, setSelectedTime] = useState(new Date());
   const [duration, setDuration] = useState("5");
 
   return (
@@ -22,7 +39,7 @@ export default AddPumpTask = function ({ navigation }) {
       <View style={styles.lottielogo}>
         <LottieView
           style={styles.pumpImage}
-          source={require("../../assets/light.json")}
+          source={require("../../assets/watering.json")}
           autoPlay
           loop
           speed={2}
@@ -48,7 +65,27 @@ export default AddPumpTask = function ({ navigation }) {
       </View>
       
       <View style={styles.pickerContainer}>
-        <Text style={styles.textPicker}>Pick Time For Pump</Text>
+        <Text style={styles.textPicker}>Select start time and duration</Text>
+        <View style={styles.selectCalendar}>
+          <View style={styles.selectIcon}>
+            <TouchableOpacity onPress={showTimePicker}>
+              <Icon name="clock-o" size={30} color="#F7AFAF" />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.selectText}>
+            {selectedTime ? (
+              <Text style={styles.selectedDateText}>
+                {selectedTime ? moment(selectedTime).format("hh:mm A") : null}
+              </Text>
+            ) : null}
+            <DateTimePickerModal
+              isVisible={isTimePickerVisible}
+              mode="time"
+              onConfirm={handleTimeConfirm}
+              onCancel={handleTimeCancel}
+            />
+          </View>
+        </View>
         <Picker
           selectedValue={duration}
           onValueChange={(itemValue, itemIndex) => setDuration(itemValue)}
@@ -85,16 +122,16 @@ const styles = StyleSheet.create({
     flex: 1.5,
   },
   pumpImage: {
-    marginTop:'5%',
-    width: 0.3 * windowWidth,
+    marginTop:'2.5%',
+    width: 0.25 * windowWidth,
     height: 0.25 * windowHeight,
-    marginBottom: 30,
+    marginBottom: 20,
     alignSelf:'center',
   },
   // CSS Topic's Name
   topic: {
     flex: 2,
-    flexGrow: 0.3,
+    flexGrow: 0.4,
     flexDirection: "row",
     borderColor: "#fff",
     borderWidth: 1,
@@ -106,12 +143,37 @@ const styles = StyleSheet.create({
   },
   // CSS picker
   pickerContainer: {
-    flex: 2,
+    flex: 2.5,
     marginTop: 20,
     alignItems: "center",
     height: 0.3 * windowHeight,
     width: 1 * windowWidth,
+    // backgroundColor:'aqua',
   },
+  // CSS Time Picker
+  selectCalendar: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingRight: 60,
+  },
+  selectIcon: {
+    backgroundColor: "#52B640",
+    borderRadius: 15,
+    height: 50,
+    width: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  selectText: {
+    marginLeft: 20,
+    textAlign: "right",
+  },
+  selectedDateText: {
+    fontSize: 20,
+    fontWeight: "700",
+    color: "#329B24",
+  },
+  // CSS scroll time 
   pickerItem: {
     fontSize: 24,
     fontWeight: "500",
